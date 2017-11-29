558172bfca55807a9573cbf273c7edb83217a990
temp <temp>
2017-11-29 13:10:55 -0500

    star

--- DrawStar/Form1.cs
+++ DrawStar/Form1.cs
@@ -10,22 +10,41 @@ using System.Windows.Forms;
 
 namespace DrawStar
 {
-    public partial class DrawStar : Form
+    public partial class Star : Form
     {
-        public DrawStar()
+        public Star()
+
         {
             InitializeComponent();
+
         }
+
+
 
         private void button1_Click(object sender, EventArgs e)
         {
+            float x = Convert.ToSingle(xBox.Text);
+            float y = Convert.ToSingle(yBox.Text);
+            float size =  Convert.ToSingle(sizeBox.Text);
             Pen starPen = new Pen(Color.Yellow);
+            DrawStar(starPen, x, y, size);
         }
-    }
 
-    public void DrawStar(int x, int y, int Size)
-    {
-        
-    }
-    
+        public void DrawStar(Pen pen, float x, float y, float size)
+        {
+            float scale = size / 90;
+            Graphics formGraphics = this.CreateGraphics();
+            formGraphics.Clear(Color.Black);
+            formGraphics.DrawLine(pen, 0 *scale + x, 35 * scale + y, 35 * scale + x, 35 * scale + y);
+            formGraphics.DrawLine(pen, 35 * scale + x, 35 * scale + y, 45 * scale + x, 0 * scale + y);
+            formGraphics.DrawLine(pen, 45 * scale + x, 0 * scale + y, 55 * scale + x, 35 * scale + y);
+            formGraphics.DrawLine(pen, 55 * scale + x, 35 * scale + y, 90 * scale + x, 35 * scale + y);
+            formGraphics.DrawLine(pen, 90 * scale + x, 35 * scale + y, 60 * scale + x, 55 * scale + y);
+            formGraphics.DrawLine(pen, 60 * scale + x, 55 * scale + y, 70 * scale + x, 85 * scale + y);
+            formGraphics.DrawLine(pen, 70 * scale + x, 85 * scale + y, 45 * scale + x, 65 * scale + y);
+            formGraphics.DrawLine(pen, 45 * scale + x, 65 * scale + y, 17 * scale + x, 85 * scale + y);
+            formGraphics.DrawLine(pen, 17 * scale + x, 85 * scale + y, 30 * scale + x, 55 * scale + y);
+            formGraphics.DrawLine(pen, 30 * scale + x, 55 * scale + y, 0 * scale + x, 35 * scale + y);
+        }
+    } 
 }
